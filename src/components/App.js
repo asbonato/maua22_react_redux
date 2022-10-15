e5be00a44b1952cfd099b10470eb83c2e23f419e
asbonato <asbonato@gmail.com>
2022-10-15 12:16:58 -0300

    apostila 9 react finalizada

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PessoaDetalhe from './PessoaDetalhe'
+import PessoaLista from './PessoaLista'
+
+const App = () => {
+    return (
+        <div className='grid border border-1 border-200 m-2'>
+            <div className="col-6">
+                <PessoaLista />
+            </div>
+            <div className="col-6">
+                <PessoaDetalhe />
+            </div>
+        </div>
+    )
+}
+export default App
